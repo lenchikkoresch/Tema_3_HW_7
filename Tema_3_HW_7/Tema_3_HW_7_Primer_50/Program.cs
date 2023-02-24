00593548aefe5aab1ae3b2a6a706530c59487fd8
lenschik.kor <lenschik.kor@yandex.ru>
2023-02-24 05:09:27 +0300

    Задача_50_условие_решение

--- Tema_3_HW_7/Tema_3_HW_7_Primer_50/Program.cs
+++ Tema_3_HW_7/Tema_3_HW_7_Primer_50/Program.cs
@@ -14,3 +14,80 @@
 */
 
 
+int GetNumber(string message)
+{
+int result = 0;
+
+while(true)
+{
+Console.WriteLine(message);
+
+if(int.TryParse(Console.ReadLine(), out result) && result > 0)
+{
+break;
+}
+else
+{
+Console.WriteLine("Ввели не корректное число. Повторите ввод.");
+}
+}
+
+return result;
+}
+
+
+
+int[,] InitMatrix(int rows, int columns)
+{
+int[,] matrix = new int[rows, columns];
+Random rnd = new Random();
+
+for (int i = 0; i < rows; i++)
+{
+for (int j = 0; j < columns; j++)
+{
+matrix[i,j] = rnd.Next(-99, 100);
+}
+}
+
+return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+for (int j = 0; j < matrix.GetLength(1); j++)
+{
+Console.Write($"{matrix[i,j]} ");
+}
+
+Console.WriteLine();
+}
+}
+
+int countOfRows = GetNumber("Введите кол-во строк:");
+
+int countOfColumns = GetNumber("Введите кол-во столбцов:");
+
+int[,] matrix = InitMatrix(countOfRows, countOfColumns);
+
+Console.WriteLine();
+
+PrintMatrix(matrix);
+
+Console.WriteLine();
+int n = GetNumber("Введите номер строки искомого элемента");
+int m = GetNumber("Введите номер столбца искомого элемента");
+
+if (n > matrix.GetLength(0) || m > matrix.GetLength(1))
+{
+    Console.WriteLine();
+    Console.WriteLine("Такого элемента нет");
+}
+else
+{
+    Console.WriteLine();
+    Console.WriteLine($"Значение элемента {n} строки и {m} столбца равно {matrix[n-1,m-1]}");
+}
+
